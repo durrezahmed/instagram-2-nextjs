8114ab1786658ce39afb7d5b13a0d63ac5fbb3a2
Durrez Ahmed <durrezahmed3@gmail.com>
2022-05-06 19:34:52 +0530

    build the post component (1)

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,5 +1,19 @@
-function Post() {
-  return <div>Post</div>;
+function Post({ id, username, userImg, img, caption }) {
+  return (
+    <div>
+      {/* Header */}
+
+      {/* Image */}
+
+      {/* Buttons */}
+
+      {/* Captions */}
+
+      {/* Comments */}
+
+      {/* Input Box */}
+    </div>
+  );
 }
 
 export default Post;
