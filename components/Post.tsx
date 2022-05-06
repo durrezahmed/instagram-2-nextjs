23e63d52f40f75dc6b616429e0e4d93fc364cff1
Durrez Ahmed <durrezahmed3@gmail.com>
2022-05-06 19:55:03 +0530

    build the header section of the post

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,7 +1,37 @@
-function Post({ id, username, userImg, img, caption }) {
+import {
+  BookmarkIcon,
+  ChatIcon,
+  DotsHorizontalIcon,
+  EmojiHappyIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+} from '@heroicons/react/outline';
+
+function Post({
+  id,
+  username,
+  userImg,
+  img,
+  caption,
+}: {
+  id: any;
+  username: any;
+  userImg: any;
+  img: any;
+  caption: any;
+}) {
   return (
-    <div>
+    <div className='my-7 rounded-sm border bg-white'>
       {/* Header */}
+      <div className='flex items-center p-5'>
+        <img
+          src={userImg}
+          className='mr-3 h-12 w-12 rounded-full border object-contain p-1'
+          alt=''
+        />
+        <p className='flex-1 font-bold'>{username}</p>
+        <DotsHorizontalIcon className='h-5 cursor-pointer' />
+      </div>
 
       {/* Image */}
 
