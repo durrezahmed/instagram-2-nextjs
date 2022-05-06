f823d31f9e57808257adb5cd6b84a83157d23732
Durrez Ahmed <durrezahmed3@gmail.com>
2022-05-06 17:45:07 +0530

    build the feed component

---
+++ components/Feed.tsx
@@ -0,0 +1,17 @@
+function Feed() {
+  return (
+    <main>
+      <section>
+        {/* Stories */}
+        {/* Posts */}
+      </section>
+
+      <section>
+        {/* Mini Profile */}
+        {/* Suggestions */}
+      </section>
+    </main>
+  );
+}
+
+export default Feed;
