fc192c35adaa789bd418fa7899be7a7c713f59fa
Durrez Ahmed <durrezahmed3@gmail.com>
2022-05-06 17:51:41 +0530

    build the stories section of the feed (1)

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,8 +1,12 @@
+import Stories from './Stories';
+
 function Feed() {
   return (
-    <main>
-      <section>
+    <main className='mx-auto grid grid-cols-1 md:max-w-3xl md:grid-cols-2 xl:max-w-6xl xl:grid-cols-3'>
+      <section className='col-span-2'>
         {/* Stories */}
+        <Stories />
+
         {/* Posts */}
       </section>
 
